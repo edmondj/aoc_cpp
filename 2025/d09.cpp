d39b69a1880db1060970120bca02ba71b07888c7
edmondj <julien.edmond@epitech.eu>
2025-12-09 15:11:44 -0500

    Optimizing d09

--- 2025/d09.cpp
+++ 2025/d09.cpp
@@ -17,11 +17,24 @@ public:
     assert(from.x() == to.x() || from.y() == to.y());
   }
 
+  constexpr auto operator<=>(const segment_t &) const = default;
+
   const point_t &from() const { return m_from; }
   const point_t &to() const { return m_to; }
 
 private:
   point_t m_from, m_to;
+};
+
+template <> struct std::hash<segment_t> {
+  size_t operator()(const segment_t &s) const noexcept {
+    auto sub_hasher = std::hash<point_t>{};
+
+    aoc::hash_accumulator acc;
+    acc.accumulate(sub_hasher(s.from()));
+    acc.accumulate(sub_hasher(s.to()));
+    return acc.result();
+  }
 };
 
 bool polygon_contains(std::span<const segment_t> segments, point_t p,
@@ -34,14 +47,12 @@ bool polygon_contains(std::span<const segment_t> segments, point_t p,
   for (const auto &s : segments) {
     if (s.from().y() == s.to().y()) {
       if (s.from().y() == p.y()) {
-        auto [low_x, high_x] = std::minmax(s.from().x(), s.to().x());
-        if (low_x <= p.x() && p.x() <= high_x) {
+        if (s.from().x() <= p.x() && p.x() <= s.to().x()) {
           return true;
         }
       }
     } else if (s.from().x() >= p.x()) {
-      auto [low_y, high_y] = std::minmax(s.from().y(), s.to().y());
-      if (low_y <= p.y() && p.y() <= high_y) {
+      if (s.from().y() <= p.y() && p.y() <= s.to().y()) {
         if (s.from().x() == p.x()) {
           return true;
         }
@@ -58,23 +69,29 @@ bool polygon_contains(std::span<const segment_t> segments,
                       const std::set<point_t::value_type> &x_range,
                       const std::set<point_t::value_type> &y_range,
                       const segment_t &test,
-                      std::unordered_map<point_t, bool> &cache) {
-  if (test.from().x() == test.to().x()) {
-    auto [low_y, high_y] = std::minmax(test.from().y(), test.to().y());
-    return std::ranges::all_of(
-        std::ranges::subrange(y_range.lower_bound(low_y),
-                              y_range.upper_bound(high_y - 1)),
-        [&cache, segments, x = test.from().x()](point_t::value_type y) {
-          return polygon_contains(segments, {x, y + 1}, cache);
-        });
+                      std::unordered_map<segment_t, bool> &segment_cache,
+                      std::unordered_map<point_t, bool> &point_cache) {
+  if (auto found = segment_cache.find(test); found != segment_cache.end()) {
+    return found->second;
   }
-  auto [low_x, high_x] = std::minmax(test.from().x(), test.to().x());
-  return std::ranges::all_of(
-      std::ranges::subrange(x_range.lower_bound(low_x),
-                            x_range.upper_bound(high_x - 1)),
-      [&cache, segments, y = test.from().y()](point_t::value_type x) {
-        return polygon_contains(segments, {x + 1, y}, cache);
-      });
+  bool res = [&]() {
+    if (test.from().x() == test.to().x()) {
+      return std::ranges::all_of(
+          std::ranges::subrange(y_range.lower_bound(test.from().y()),
+                                y_range.upper_bound(test.to().y() - 1)),
+          [&point_cache, segments, x = test.from().x()](point_t::value_type y) {
+            return polygon_contains(segments, {x, y + 1}, point_cache);
+          });
+    }
+    return std::ranges::all_of(
+        std::ranges::subrange(x_range.lower_bound(test.from().x()),
+                              x_range.upper_bound(test.to().x() - 1)),
+        [&point_cache, segments, y = test.from().y()](point_t::value_type x) {
+          return polygon_contains(segments, {x + 1, y}, point_cache);
+        });
+  }();
+  segment_cache.insert_or_assign(test, res);
+  return res;
 }
 
 struct d09 {
@@ -119,34 +136,37 @@ struct d09 {
       x_range.insert(p.x());
       y_range.insert(p.y());
     }
-    auto cache = std::unordered_map<point_t, bool>{};
+    auto segment_cache = std::unordered_map<segment_t, bool>{};
+    auto point_cache = std::unordered_map<point_t, bool>{};
+    auto area = point_t::value_type{0};
 
-    return std::ranges::max(
-        std::views::iota(0uz, input.size() - 1) |
-        std::views::transform([&cache, &x_range, &y_range, &input,
-                               &segments](size_t i) {
-          return std::ranges::subrange(input.begin() + i + 1, input.end()) |
-                 std::views::transform([&cache, &x_range, &y_range,
-                                        cur = input[i],
-                                        &segments](const point_t &next) {
-                   auto extra_corners = std::array{point_t{cur.x(), next.y()},
-                                                   point_t{next.x(), cur.y()}};
-                   if (polygon_contains(segments, x_range, y_range,
-                                        {cur, extra_corners[0]}, cache) &&
-                       polygon_contains(segments, x_range, y_range,
-                                        {extra_corners[0], next}, cache) &&
-                       polygon_contains(segments, x_range, y_range,
-                                        {next, extra_corners[1]}, cache) &&
-                       polygon_contains(segments, x_range, y_range,
-                                        {extra_corners[1], cur}, cache)) {
-                     return (abs(cur.x() - next.x()) + 1) *
-                            (abs(cur.y() - next.y()) + 1);
-                   } else {
-                     return 0ll;
-                   }
-                 });
-        }) |
-        std::views::join);
+    for (size_t i = 0; i < input.size() - 1; ++i) {
+      auto cur = input[i];
+      for (auto next :
+           std::ranges::subrange(input.begin() + i + 1, input.end())) {
+        auto new_area =
+            (abs(cur.x() - next.x()) + 1) * (abs(cur.y() - next.y()) + 1);
+        if (new_area > area) {
+          auto extra_corners = std::array{point_t{cur.x(), next.y()},
+                                          point_t{next.x(), cur.y()}};
+          if (polygon_contains(segments, x_range, y_range,
+                               {cur, extra_corners[0]}, segment_cache,
+                               point_cache) &&
+              polygon_contains(segments, x_range, y_range,
+                               {extra_corners[0], next}, segment_cache,
+                               point_cache) &&
+              polygon_contains(segments, x_range, y_range,
+                               {next, extra_corners[1]}, segment_cache,
+                               point_cache) &&
+              polygon_contains(segments, x_range, y_range,
+                               {extra_corners[1], cur}, segment_cache,
+                               point_cache)) {
+            area = new_area;
+          }
+        }
+      }
+    }
+    return area;
   }
 };
 
@@ -160,26 +180,15 @@ AOC_MAIN(d09)
 
 #include <gtest/gtest.h>
 
-// const auto TEST_DATA = aoc::arguments::make_example(R"(7,1
-// 11,1
-// 11,7
-// 9,7
-// 9,5
-// 2,5
-// 2,3
-// 7,3)");
-// TEST(d09, part1) { EXPECT_EQ(aoc::part1<d09>(TEST_DATA), 50); }
-// TEST(d09, part2) { EXPECT_EQ(aoc::part2<d09>(TEST_DATA), 24); }
-
-const auto TEST_DATA = aoc::arguments::make_example(R"(1,1
-10,1
-10,10
-1,10
-1,7
-8,7
-8,5
-1,5)");
-TEST(d09, part1) { EXPECT_EQ(aoc::part1<d09>(TEST_DATA), 100); }
-TEST(d09, part2) { EXPECT_EQ(aoc::part2<d09>(TEST_DATA), 50); }
+const auto TEST_DATA = aoc::arguments::make_example(R"(7,1
+11,1
+11,7
+9,7
+9,5
+2,5
+2,3
+7,3)");
+TEST(d09, part1) { EXPECT_EQ(aoc::part1<d09>(TEST_DATA), 50); }
+TEST(d09, part2) { EXPECT_EQ(aoc::part2<d09>(TEST_DATA), 24); }
 
 #endif
